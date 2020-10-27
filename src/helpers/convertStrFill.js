8cd9a41a255bb3c125648283ff761f46fe2bfa3e
Radosvet Petrov <93radosvet@gmail.com>
2020-10-27 20:12:12 +0000

    better logic

--- src/helpers/convertStrFill.js
+++ src/helpers/convertStrFill.js
@@ -1,11 +1,11 @@
 export const convertStrFill = (word, fill) => {
   const filledArr = word && String(word) && word.split("").fill(fill);
   const firstLetter = word[0];
-  
+
   word
     .split("")
-    .map((character, index) => {
-      if (character === firstLetter) return index;
+    .map((character, index) =>{
+      return character === firstLetter && index;
     })
     .filter((index) => (filledArr[index] = firstLetter.toUpperCase()));
 
